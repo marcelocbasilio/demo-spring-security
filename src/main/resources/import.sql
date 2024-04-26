675ff633fef3d01c0f8856e16c85b6c49e36cb8d
Marcelo Basílio <marcelocbasilio@gmail.com>
2024-04-25 23:36:18 -0300

    Adding BCrypt password encoder to the project.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$UySGk1KHvbKIiBuLYGCJee37utE2MtKhtTxBGe2h67C7oXTAhIHXu');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$UySGk1KHvbKIiBuLYGCJee37utE2MtKhtTxBGe2h67C7oXTAhIHXu');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
