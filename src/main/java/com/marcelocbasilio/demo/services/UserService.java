eb95c13f8dd1580a46cd88382f77ae8559f26bce
Marcelo Basílio <marcelocbasilio@gmail.com>
2024-05-03 09:59:27 -0300

    Implementing Spring Security.

---
+++ src/main/java/com/marcelocbasilio/demo/services/UserService.java
@@ -0,0 +1,37 @@
+package com.marcelocbasilio.demo.services;
+
+import com.marcelocbasilio.demo.entities.Role;
+import com.marcelocbasilio.demo.entities.User;
+import com.marcelocbasilio.demo.projections.UserDetailsProjection;
+import com.marcelocbasilio.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+        return user;
+    }
+}
